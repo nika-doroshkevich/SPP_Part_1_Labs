fa7e5235bbbd5fc93afa6a6154cd75fb75331290
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2022-11-30 02:58:18 +0300

    SPP Lab 3

---
+++ Lab_3/TestClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using AssemblyBrowserLib;
+
+namespace TestClass
+{
+    internal static class Program
+    {
+        private static readonly AssemblyBrowser Browser = new AssemblyBrowser();
+        
+        public static void Main(string[] args)
+        {
+            
+            var assemblyInfo = Browser.GetAssemblyInfo("Path\\TestClass.exe");
+            
+        }
+        
+    }
+    
+    public static class StringExtension
+    {
+        public static int CharCount(this string str, char c)
+        {
+            int counter = 0;
+            for (int i = 0; i<str.Length; i++)
+            {
+                if (str[i] == c)
+                    counter++;
+            }
+            return counter;
+        }
+    }
+}
---
+++ Lab_3/Tests/Tests.cs
@@ -0,0 +1,68 @@
+﻿using AssemblyBrowserLib;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private readonly AssemblyBrowser _browser = new AssemblyBrowser();
+        private const string TestDirectory = "Path\\Tests\\TestFiles\\";
+
+        [Test]
+        public void DllBrowserWorkFinishedCorrectly()
+        {
+            _browser.GetAssemblyInfo(TestDirectory+"AssemblyBrowserLib.dll");
+            
+            Assert.True(true);
+        }
+        
+        [Test]
+        public void ExeBrowserWorkFinishedCorrectly()
+        {
+            _browser.GetAssemblyInfo(TestDirectory+"TestClass.exe");
+            
+            Assert.True(true);
+        }
+        
+        [Test]
+        public void ExeBrowserWorkFinishedCorrectly2()
+        {
+            _browser.GetAssemblyInfo(TestDirectory+"View.exe");
+            
+            Assert.True(true);
+        }
+        
+        [Test]
+        public void NameSpaceIsCorrect()
+        {
+            var assemblyInfo = _browser.GetAssemblyInfo(TestDirectory+"TestClass.exe");
+            ;
+            Assert.IsTrue(assemblyInfo[0].Signature.Equals("TestClass") );
+        }
+        
+        [Test]
+        public void ClassIsCorrect()
+        {
+            var assemblyInfo = _browser.GetAssemblyInfo(TestDirectory+"TestClass.exe");
+            ;
+            Assert.IsTrue(assemblyInfo[0].Class.Equals("public  class  A") );
+        }
+        
+        [Test]
+        public void MethodAmountIsCorrect()
+        {
+            var assemblyInfo = _browser.GetAssemblyInfo(TestDirectory+"TestClass.exe");
+            ;
+            Assert.AreEqual(assemblyInfo[0].Members.Count, 3);
+        }
+        
+        [Test]
+        public void MethodNotNull()
+        {
+            var assemblyInfo = _browser.GetAssemblyInfo(TestDirectory+"TestClass.exe");
+            ;
+            Assert.NotNull(assemblyInfo[1].Members);
+        }
+    }
+}
